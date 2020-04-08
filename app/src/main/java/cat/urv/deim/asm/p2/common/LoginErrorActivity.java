70cbda577bc2f9f488e0c8028f83d3e9fa7582ee
Aleix Vives <aleix.vives@estudiants.urv.cat>
2020-04-08 21:01:06 +0200

    Created LoginError screen and activity.

---
+++ app/src/main/java/cat/urv/deim/asm/p2/common/LoginErrorActivity.java
@@ -0,0 +1,12 @@
+package cat.urv.deim.asm.p2.common;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginErrorActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
