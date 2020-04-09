c203876e0dbff6da3e0f19f105e3de0ef2f21b68
Adam Alvarado <d3ltcod@protonmail.com>
2020-04-09 12:22:24 +0200

    Added a Splash Screen

---
+++ app/src/main/java/cat/urv/deim/asm/p2/common/SplashActivity.java
@@ -0,0 +1,29 @@
+package cat.urv.deim.asm.p2.common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+               goToMainActivity();
+            }
+        }, 2000);   //2 seconds
+    }
+
+    private void goToMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
