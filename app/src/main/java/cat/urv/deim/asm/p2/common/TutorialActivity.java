2f91910046872aa5f4db6c40f404983d83ad0490
Adam Alvarado <d3ltcod@protonmail.com>
2020-04-10 19:31:32 +0200

    Added progress bar

--- app/src/main/java/cat/urv/deim/asm/p2/common/TutorialActivity.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/TutorialActivity.java
@@ -2,20 +2,23 @@ package cat.urv.deim.asm.p2.common;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
-
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.ProgressBar;
 import cat.urv.deim.asm.p2.common.adapters.ViewPageAdapter;
 
 public class TutorialActivity extends AppCompatActivity {
 
+    private ProgressBar progressBar;
     private ViewPager viewPager;
     private int[] layouts = {R.layout.tutorial_screen_1, R.layout.tutorial_screen_2, R.layout.tutorial_screen_3};
+    private int currentPercent = 0;
+    private final int percentProgress = 100/layouts.length;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,9 @@ public class TutorialActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.tutorial_viewPager);
         ViewPageAdapter adapter = new ViewPageAdapter(layouts, getApplicationContext());
         viewPager.setAdapter(adapter);
+
+        progressBar = findViewById(R.id.tutorial_progressBar);
+        progressBar.setSecondaryProgress(currentPercent+percentProgress);
 
         Button skipBtn = findViewById(R.id.skip_tutorial_btn);
         Button nextBtn = findViewById(R.id.next_tutorial_btn);
@@ -43,9 +49,14 @@ public class TutorialActivity extends AppCompatActivity {
     // Create an anonymous implementation of OnClickListener for next button
     private View.OnClickListener nextListener = new View.OnClickListener() {
         public void onClick(View v) {
+            fillProgress(progressBar, currentPercent, currentPercent+percentProgress);
+            currentPercent += percentProgress;
+
             int currentPage = viewPager.getCurrentItem()+1;
+
             if(currentPage < layouts.length) {
                 viewPager.setCurrentItem(currentPage);
+                progressBar.setSecondaryProgress(currentPercent+percentProgress);
             } else {
                 startMainActivity();
             }
@@ -65,5 +76,11 @@ public class TutorialActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private void fillProgress(ProgressBar pb, int start, int end) {
+        ObjectAnimator progressAnimator = ObjectAnimator.ofInt(pb, "progress", start, end);
+        progressAnimator.setDuration(1000);
+        progressAnimator.start();
     }
 }
