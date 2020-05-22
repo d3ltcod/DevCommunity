6f94b2179af9d1c4108ff6233d383d4db3084637
Adam Alvarado <d3ltcod@protonmail.com>
2020-05-22 18:27:09 +0200

    Added list of cards (Articles)

--- app/src/main/java/cat/urv/deim/asm/p2/common/ui/articles/ArticlesFragment.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/ui/articles/ArticlesFragment.java
@@ -8,8 +8,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.Objects;
+
+import cat.urv.deim.asm.libraries.commanagerdc.providers.DataProvider;
 import cat.urv.deim.asm.p2.common.R;
+import cat.urv.deim.asm.p3.shared.adapters.ArticlesListAdapter;
 
 public class ArticlesFragment extends Fragment {
 
@@ -19,6 +25,22 @@ public class ArticlesFragment extends Fragment {
                              Bundle savedInstanceState) {
         articlesViewModel = ViewModelProviders.of(this).get(ArticlesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_articles, container, false);
+
+        RecyclerView recyclerView = root.findViewById(R.id.card_list);
+        
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        //Load data from commanagerdc
+        DataProvider dataProvider = DataProvider.getInstance(Objects.requireNonNull(getActivity()).getApplicationContext(),R.raw.faqs,R.raw.news,R.raw.articles,R.raw.events,R.raw.calendar);
+
+        // specify an adapter (see also next example)
+        RecyclerView.Adapter cardListAdapter = new ArticlesListAdapter(dataProvider.getArticles());
+        recyclerView.setAdapter(cardListAdapter);
 
         return root;
     }
---
+++ app/src/main/java/cat/urv/deim/asm/p3/shared/adapters/ArticlesListAdapter.java
@@ -0,0 +1,70 @@
+package cat.urv.deim.asm.p3.shared.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+import cat.urv.deim.asm.libraries.commanagerdc.models.Article;
+import cat.urv.deim.asm.p2.common.R;
+
+public class ArticlesListAdapter extends RecyclerView.Adapter<ArticlesListAdapter.ArticleListViewHolder> {
+
+    private List<Article> dataList;
+
+    static class ArticleListViewHolder extends RecyclerView.ViewHolder {
+
+        TextView titleTextView;
+        TextView subTitleTextView;
+        TextView authorTextView;
+        ImageView imageView;
+        ToggleButton favoriteToggleButton;
+        ToggleButton markToggleButton;
+
+        ArticleListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.title_text);
+            subTitleTextView = itemView.findViewById(R.id.subtitle_text);
+            authorTextView = itemView.findViewById(R.id.author_text);
+            imageView = itemView.findViewById(R.id.media_image);
+            favoriteToggleButton = itemView.findViewById(R.id.favorite_button);
+            markToggleButton = itemView.findViewById(R.id.mark_button);
+        }
+    }
+
+    public ArticlesListAdapter(List<Article>  dataList) {
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public ArticlesListAdapter.ArticleListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View rootView = LayoutInflater.from(parent.getContext()).inflate(R.layout.article_card_view,parent,false);
+        return new ArticleListViewHolder(rootView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ArticlesListAdapter.ArticleListViewHolder articleListViewHolder, int position) {
+        Article article = dataList.get(position);
+
+        articleListViewHolder.titleTextView.setText(article.getTitle());
+        if(article.getAbstractText().length() > 50)
+            articleListViewHolder.subTitleTextView.setText(article.getAbstractText().substring(0, 50));
+        else
+            articleListViewHolder.subTitleTextView.setText(article.getAbstractText());
+        articleListViewHolder.authorTextView.setText(article.getAuthor());
+        Picasso.get().load(article.getImageURL()).into(articleListViewHolder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+}
